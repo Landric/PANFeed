ce925efff95477191278f707e6b437c01c661872
Tom Blount <tb12g09@ecs.soton.ac.uk>
2012-07-10 14:56:22 +0100

    Added ability to add PANFeeds directly to Digests (in ManageDigest form)

--- personalise/staticfiles/scripts/manage_digest.js
+++ personalise/staticfiles/scripts/manage_digest.js
@@ -21,3 +21,15 @@ function addAnother()
 	group.appendTo('#feed_urls');
 	
 }
+
+function addPANFeed()
+{
+	var keywords = $("#keyword_input").val().replace(/[^0-9a-zA-Z]/g,",").split(",");
+	var url = "http://panfeed.ecs.soton.ac.uk/find/all/" + keywords.join("_");
+	
+	var group = $('#feed_urls').find('.control-group:first').clone();
+	group.find('input').val(url);
+	group.find('.remove').bind('click', removeFeed);
+	group.find('.add').bind('click', addAnother);
+	group.appendTo('#feed_urls');
+}
