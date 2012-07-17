2f976d2b04817ce6a6313532750032f4c828e1ae
Thomas Grainger <tagrain@gmail.com>
2012-07-17 14:38:00 +0100

    added request context processor

--- settings.py
+++ settings.py
@@ -134,6 +134,19 @@ INSTALLED_APPS = (
     'registration',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS =(
+	"django.contrib.auth.context_processors.auth",
+	"django.core.context_processors.debug",
+	"django.core.context_processors.i18n",
+	"django.core.context_processors.media",
+	"django.core.context_processors.static",
+	"django.contrib.messages.context_processors.messages"
+)
+
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'django.core.context_processors.request'
+)
+
 
 EMAIL_HOST = "smtp.ecs.soton.ac.uk"
 
