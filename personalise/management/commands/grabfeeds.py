1a229ee64e44b84cda0be175855fdcf12de9d1f0
Thomas Grainger <tagrain@gmail.com>
2012-07-19 10:19:47 +0100

    remove duplicate code from grabfeeds

--- personalise/management/commands/grabfeeds.py
+++ personalise/management/commands/grabfeeds.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from personalise.models import AcademicFeeds, Corpus, Corpuskeywords, Tf, Words
+from corpus import Corpus as CorpusScraper
 from django.db import connection
 import feedparser
 import urllib2
@@ -20,14 +21,13 @@ class Command(BaseCommand):
         corp.count_words_and_store()
         corp.calculate_keywords_for_all()
 
-class corpus_obj():
+class corpus_obj(CorpusScraper):
     
-                
     def build_corpus(self):
         #### Builds a corpus of documents from a set of feeds.
     
         #c.execute("""SELECT url,toplevel FROM feeds""")
-        feeds = AcademicFeeds.objects.all().values_list['url' flat=True]
+        feeds = AcademicFeeds.objects.all().values_list('url', flat=True)
         for feedurl in feeds:
             print feedurl
             try:
@@ -43,77 +43,3 @@ class corpus_obj():
                         Corpus.objects.create(title=item.title,description=item.description,url=item.link,feed=feedurl,length=len(item.description+item.title),date=d,toplevel=feedurl.toplevel)
             except urllib2.URLError:
                 print "Error getting page: ", feedurl
-        
-    def count_words_and_store(self):
-        ### Performs a wordcount of each document and stores cumulative word count 
-        ### and also wordcount specific to that document/word combination.
-        corpusses = Corpus.objects.all()
-        for corpus in corpusses:
-            if not Tf.objects.filter(corpus=corpus).exists():
-                totaltext = unicodedata.normalize('NFKD',(corpus.title+' '+corpus.description)).encode('ascii','ignore')
-                totaltext = self.__remove_extra_spaces(self.__remove_html_tags(totaltext))
-                freq = self.__get_word_frequencies(self.__remove_single_characters((''.join((x for x in (totaltext.lower()) if x not in string.punctuation)))))
-                for word in freq:
-                    itemid = corpus.id
-                    #print word, " ", freq[word], " ", itemid
-                    dbword,created = Words.objects.get_or_create(word=word, defaults={'count':0})
-                    dbword.count += 1
-                    dbword.save()
-                    Tf(word = dbword, corpus = corpus, count = freq[word]).save()                
-                
-            
-    def __get_word_frequencies(self,text):
-        result = dict([(w[:29], text.count(w[:29])) for w in text.split()])
-        return result
-        
-    def __remove_html_tags(self,data):
-        p = re.compile(r'<.*?>')
-        return p.sub('', data)
-
-    def __remove_extra_spaces(self,data):
-        p = re.compile(r'\s+')
-        return p.sub(' ', data)
-        
-    def __remove_single_characters(self,data):
-        p=re.compile(r'(^|\s)[^ \t]($|\s)')
-        return p.sub('',data)
-        
-    def calculate_keywords_for_all(self):
-        corpusses = Corpus.objects.all()
-        global corpussize
-        corpussize = Corpus.objects.count()
-        
-        for corpus in corpusses:
-            if not Corpuskeywords.objects.filter(corpus=corpus).exists():
-                ### For each item from feeds
-                worddata = {}
-                words = Tf.objects.filter(corpus=corpus).values('word','count')
-                ### For each word in that item
-                for word in words:
-                    wordcount = Words.objects.get(word['word']).count
-                    worddata[word['word']] = (word['count'], wordcount)
-                keywords = self.calculate_tfidf(worddata)
-                topkeys = keywords[:10]
-                for rank, word in topkeys:
-                    Corpuskeywords(corpus=corpus, word=word, rank=rank).save()                
-        
-    def calculate_tfidf(self,worddata):
-        wordlist = []
-        for word in worddata:
-            idf = math.log(corpussize/(worddata[word][1]+1.0))
-            tfidf = idf * worddata[word][0]
-            wordlist.append((tfidf,word))
-        return sorted(wordlist, reverse=True)
-        
-    def get_keywords_for_item(self,itemid):
-        words = Corpuskeywords.objects.filter(corpus__id=itemid).values_list('word', flat=True)
-        print words
-        return words
-        
-    def find_matching_items(self,keywords):
-        result = set()
-        for key in keywords:
-            matches = Corpuskeywords.objects.filter(word__contains="foo").values("corpus", "corpus__title")
-            for listitem in matches:
-                result.add(listitem)
-        print result
