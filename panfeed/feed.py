263d68c314e4efd9682e77fac35358b2841f3575
Thomas Grainger <tagrain@gmail.com>
2012-07-20 11:52:40 +0100

    feed now takes into account url parameter Fixes #14

--- panfeed/feed.py
+++ panfeed/feed.py
@@ -5,7 +5,7 @@ from panfeed.models import Corpus
 import urllib
 
 
-from haystack.query import SearchQuerySet
+from haystack.query import SearchQuerySet, SQ
 
 from collections import namedtuple
 
@@ -15,15 +15,29 @@ class PersonalFeed(Feed):
     def items(self,obj):
 
         words = obj["keywords"]
-        #urls = params[1].split("_") 
+        urls = obj["sources"]
                 
         #urls.append(urls[0])#.append(words
         #urls.extend(words)
         
-        corpora = SearchQuerySet().models(Corpus).all()
         
+        
+        corpora = SearchQuerySet().models(Corpus)
+        cfilter = {"words":SQ(), "urls":SQ()}
         for word in words:
-            corpora = corpora.filter_or(content=word)
+            word = corpora.query.clean(word)
+            cfilter["words"] = cfilter["words"] | SQ(content=word)
+        
+        for url in urls:
+            url = corpora.query.clean(url)
+            cfilter["urls"] = cfilter["urls"] | SQ(toplevel=url)
+        
+        if urls:
+            cfilter = cfilter["words"] & cfilter["urls"]
+        else:
+            cfilter = cfilter["words"]
+        
+        corpora = corpora.filter(cfilter)
         
         return (corpus.object for corpus in corpora.load_all())
 
