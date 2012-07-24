102652416efe8b1ee6f0659486dd730170cab89c
Tom Blount <tb12g09@ecs.soton.ac.uk>
2012-07-24 15:47:49 +0100

    Updated findnews Began work on Angularising feedlist

--- panfeed/static/scripts/findnews.js
+++ panfeed/static/scripts/findnews.js
@@ -1,7 +1,15 @@
 function FindNewsCtrl($scope) {
-    $scope.searchTerms = "";
+    $scope.searchTerms = "ecs eprints";
     $scope.url = function() {
-        return URI("/find").addSearch({kw: $scope.searchTerms.split(" ")}).toString()
+        var search = $scope.searchTerms.replace(/,/g,'');
+        return URI("/find").addSearch({kw: search.split(" ")}).toString();
     };
     
+    $scope.absoluteUrl = function() {
+        return URI(document.location).path($scope.url()).toString().replace(/http::/g,'http:');
+    };
+
+    $scope.readerUrl = function() {
+        return URI(document.location).path($scope.url()).toString().replace(/http::/g,'http%3A');
+    };
 }
