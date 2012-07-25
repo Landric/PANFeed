619aa2d1c45e7b45980cccc21c625448477a4461
Thomas Grainger <tagrain@gmail.com>
2012-07-25 16:36:47 +0100

    When Search is clicked get feeds

--- panfeed/static/scripts/listfeeds.js
+++ panfeed/static/scripts/listfeeds.js
@@ -1,11 +1,16 @@
-function ListFeedsCtrl($scope) {
-    $scope.get_feeds = function(){
-	console.log("OLOLOLOLOL");
-        return $.ajax({
-                  url: '/api/v2/feed/?format=json&limit=0',
-                  type: 'GET',
-                  accepts: 'application/json',
-                  dataType: 'json'
-             }).objects;
+function ListFeedsCtrl($scope, $http, $templateCache) {
+    $scope.fetch = function(){
+    
+        $http({
+            method: "GET",
+            url: '/api/v2/feed/?format=json&limit=0',
+            cache: $templateCache,
+            transformResponse: function(data,headersGetter){
+                return JSON.parse(data).objects;
+            }
+        }).success(function(data,status){
+            $scope.feeds = data;
+        });
+        
      }
 }
