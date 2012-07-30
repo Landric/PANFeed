1a762f5256d027d332ebc7fa95ceed44a809169a
Thomas Grainger <tagrain@gmail.com>
2012-07-30 17:32:12 +0100

    indicate which field should be updated on CorpusIndex

--- panfeed/search_indexes.py
+++ panfeed/search_indexes.py
@@ -15,5 +15,8 @@ class CorpusIndex(indexes.SearchIndex):
     def index_queryset(self):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
+    
+    def get_updated_field(self):
+        return "modified"
 
 site.register(Corpus,CorpusIndex)
