8fd4d59b6597826038c377d73f86ab779261c2c8
Tom Blount <tb12g09@ecs.soton.ac.uk>
2012-08-14 16:53:48 +0100

    End of day commit API in process of being updated

--- panfeed/api/resources.py
+++ panfeed/api/resources.py
@@ -5,44 +5,36 @@ from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 from tastypie.validation import FormValidation
 from panfeed.models import Feed, FeedItem, SpecialIssue
-from panfeed.forms import FeedForm, FeedItemForm, SpecialIssueForm
+from panfeed.forms import FeedItemForm, SpecialIssueForm
 
 class DjangoAuthentication(Authentication):
     def is_authenticated(self, request, **kwargs):
-        if request.user.is_authenticated:
+        if request.method == 'GET' or request.user.is_authenticated:
           return True
 
         return False
 
-class FeedAuthorization(Authorization):
-    def apply_limits(self, request, object_list):
-        if request and hasattr(request, 'user'):
-            return object_list.filter(owner=request.user)
-
-        return object_list.none()
-
 class FeedItemAuthorization(Authorization):
     def apply_limits(self, request, object_list):
-        if request and hasattr(request, 'user'):
+        if request and request.method == 'GET':
             return object_list.filter(feed__owner=request.user)
-
+ 
+        if isinstance(object_list, Bundle):
+            if object_list.exists(feed__owner=request.user):
+                return object_list
+ 
         return object_list.none()
 
 class SpecialIssueAuthorization(Authorization):
     def apply_limits(self, request, object_list):
-        if request and hasattr(request, 'user'):
-            return object_list.filter(feed__owner=request.user)
-
-        return object_list.none()
+        pass
 
 class FeedResource(ModelResource):
     class Meta:
         resource_name = 'feed'
         queryset = Feed.objects.all()
-        allowed_methods = ['get']
+        allowed_methods = ['get', 'post', 'put']
         authentication = DjangoAuthentication()
-        authorization = FeedAuthorization()
-        validation = FormValidation(form_class=FeedForm)
 
     def dehydrate(self, bundle):
         bundle.data['url'] = bundle.obj.get_absolute_url()
@@ -52,7 +44,7 @@ class FeedItemResource(ModelResource):
     class Meta:
         resource_name = 'feeditem'
         queryset = FeedItem.objects.all()
-        allowed_methods = ['get']
+        allowed_methods = ['get', 'post', 'put']
         filtering = {
             "id": ALL,
             "special_issue": ALL,
@@ -60,6 +52,14 @@ class FeedItemResource(ModelResource):
         authentication = DjangoAuthentication()
         authorization = FeedItemAuthorization()
         validation = FormValidation(form_class=FeedItemForm)
+        
+        def obj_create(self, bundle, request, **kwargs):
+            bundle = self._meta.authorization.apply_limits(request, bundle)
+            return super(FeedItemResource, self).obj_create(bundle, request, **kwargs)
+ 
+        def obj_update(self, bundle, request, **kwargs):
+            bundle = self._meta.authorization.apply_limits(request, bundle)
+            return super(FeedItemResource, self).obj_update(bundle, request, **kwargs)
 
 class SpecialIssueResource(ModelResource):
     class Meta:
